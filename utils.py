fb61baace9fffb34ad27247d37dea1c993ce0428
Ms.Chabane <157377453+MsChabane@users.noreply.github.com>
2026-03-11 17:55:50 +0000

    add exception hander for the streaming endpoint

--- utils.py
+++ utils.py
@@ -4,21 +4,24 @@ import json
 
 
 async def stream_app_output(app,input,config):
-    async for output in app.astream(input,config=config):
-        if output.get('web search'):
-            result = json.loads(output['web search']['messages'][-1].content)
-            query = result.get('query')
-            urls = [res['url'] for res in result.get('results',[])]
-            yield f"{json.dumps({'step':'web search','query':query,'urls':urls})}\n"
-        if output.get("patient lookup"):
-            yield f"{json.dumps({'step':'patient lookup'})}\n"
-        if output.get('helper tools'):
-            yield f"{json.dumps({'step':'helper tools'})}\n"
-        if output.get('guard'):
-            yield f"{json.dumps({'step':'safety check','safety status':output['guard']['safety_status']})}\n"
-        if output.get('final answer'):
-            yield f"{json.dumps({'step':'answer written','answer':output['final answer']['answer']})}\n"
-
+    try:
+        async for output in app.astream(input,config=config):
+            if output.get('web search'):
+                result = json.loads(output['web search']['messages'][-1].content)
+                query = result.get('query')
+                urls = [res['url'] for res in result.get('results',[])]
+                yield f"{json.dumps({'step':'web search','query':query,'urls':urls})}\n"
+            if output.get("patient lookup"):
+                yield f"{json.dumps({'step':'patient lookup'})}\n"
+            if output.get('helper tools'):
+                yield f"{json.dumps({'step':'helper tools'})}\n"
+            if output.get('guard'):
+                yield f"{json.dumps({'step':'safety check','safety status':output['guard']['safety_status']})}\n"
+            if output.get('final answer'):
+                yield f"{json.dumps({'step':'answer written','answer':output['final answer']['answer']})}\n"
+    except Exception as e:
+        print(e)
+        yield f"{json.dumps({'error':'Some things went worng'})}\n"
 
 async def get_answer(app,input,config):
     output=await app.ainvoke(input,config=config)
